cf168a5890cb830592d5f0cda2781c5c848bbe81
Савва Менский <az09209@yandex.ru>
2021-04-28 21:55:55 +0700

    surya: Force triple frame buffers

--- device.mk
+++ device.mk
@@ -65,6 +65,10 @@ PRODUCT_PACKAGES += \
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
+# Settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
 # Device Settings
 $(call inherit-product-if-exists, vendor/PocoParts/pocoparts.mk)
 
